ae9108bf67768d316b166e6dfc0ea96d0ab54f0a
ryo4004 <kato@ryo4004.net>
2021-05-13 00:49:56 +0900

    今日の練習日程を取得する

--- src/script/schedule.ts
+++ src/script/schedule.ts
@@ -1,10 +1,19 @@
 import fetch from 'node-fetch'
 
-import type { Schedule } from '../types/schedule'
+import type { Schedule, EachSchedule } from '../types/schedule'
+
+const getTodaySchedule = (schedule: Schedule): EachSchedule | false => {
+  if (!schedule.today) {
+    return false
+  }
+  return schedule.next
+}
 
 // 練習日程を取得する
 ;(async () => {
   const response = await fetch('https://api.winds-n.com/schedule', { method: 'POST' })
   const schedule: Schedule = await response.json()
   console.log({ schedule })
+  const today = getTodaySchedule(schedule)
+  console.log({ today })
 })()
