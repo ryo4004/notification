50312e90a0c5a865713de58565999cf205c1f090
ryo4004 <kato@ryo4004.net>
2021-05-13 00:29:28 +0900

    練習日程の型を追加

--- src/script/schedule.ts
+++ src/script/schedule.ts
@@ -1,8 +1,10 @@
 import fetch from 'node-fetch'
 
+import type { Schedule } from '../types/schedule'
+
 // 練習日程を取得する
 ;(async () => {
   const response = await fetch('https://api.winds-n.com/schedule', { method: 'POST' })
-  const schedule = await response.json()
+  const schedule: Schedule = await response.json()
   console.log({ schedule })
 })()
