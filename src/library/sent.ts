667e5a1318d1aa71b9a368b97cc7597ca6467989
ryo4004 <kato@ryo4004.net>
2021-05-16 20:10:10 +0900

    送信履歴データベースは取得ごとに再読み込みする

--- src/library/sent.ts
+++ src/library/sent.ts
@@ -3,15 +3,17 @@ import NeDB from 'nedb'
 
 import type { SentDBData } from '../types/token'
 
-const sentDB = new NeDB({
-  filename: path.join(__dirname, '../../database/sent.db'),
-  autoload: true,
-  timestampData: true,
-})
+const getSentDB = () => {
+  return new NeDB({
+    filename: path.join(__dirname, '../../database/sent.db'),
+    autoload: true,
+    timestampData: true,
+  })
+}
 
 export const getAll = (): Promise<Array<SentDBData> | null> => {
   return new Promise((resolve) => {
-    sentDB
+    getSentDB()
       .find({})
       .sort({ createdAt: -1 })
       .exec((error: unknown, docs: Array<SentDBData>) => {
