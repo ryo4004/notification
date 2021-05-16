8886355207a882ff7ec1188268c44551e2eef096
ryo4004 <kato@ryo4004.net>
2021-05-16 13:10:55 +0900

    型を別ファイルに書く

---
+++ client/src/types/notification.ts
@@ -0,0 +1,28 @@
+type Topics = {
+  importantSchedule: boolean // 練習開始時刻に通知
+  importantManager: boolean // 事務局からのお知らせを更新したら通知
+  scheduleUpdate: boolean // 練習日程を更新したら通知
+  historyUpdate: boolean // 練習の記録を更新したら通知
+  othersUpdate: boolean // その他の更新をしたら通知
+}
+
+type TopicsKeys = keyof Topics
+
+type SentData = {
+  timestamp: string
+  topicKey: TopicsKeys
+  title: string
+  body: string
+  path: string
+  tokens: Array<string>
+  analytics: string
+  result: {
+    sendResult: unknown | null
+    sendError: unknown
+    error: string | null
+  }
+}
+
+export type SentDBData = SentData & {
+  _id: string
+}
