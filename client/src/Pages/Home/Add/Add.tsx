e5172edf951d5517fa538456f6e1aea8b2eead42
ryo4004 <kato@ryo4004.net>
2021-05-18 01:29:04 +0900

    新しい通知を追加フォームのcssを追加

--- client/src/Pages/Home/Add/Add.tsx
+++ client/src/Pages/Home/Add/Add.tsx
@@ -10,19 +10,21 @@ import './Add.scss'
 export const Add = () => {
   const { pass } = useAuthenticationContext()
   const { state, updateAdd, updateTopic, updateCheckbox, requestSend } = useAdd(pass)
+  const buttonLabel = state.notification.immediately ? 'リアルタイムで送信する' : '送信予約する'
   return (
     <div className="add">
       <details>
         <summary>
-          <h2>Add</h2>
+          <h2>新しい通知を追加</h2>
         </summary>
         <div className="details">
           <label>タイトル</label>
-          <input value={state.notification.title} onChange={(e) => updateAdd('title', e.target.value)} />
+          <input type="text" value={state.notification.title} onChange={(e) => updateAdd('title', e.target.value)} />
           <label>本文</label>
           <textarea value={state.notification.body} onChange={(e) => updateAdd('body', e.target.value)} />
           <label>パス</label>
-          <input value={state.notification.path} onChange={(e) => updateAdd('path', e.target.value)} />
+          <input type="text" value={state.notification.path} onChange={(e) => updateAdd('path', e.target.value)} />
+          <label>カテゴリ</label>
           <select value={state.notification.topic} onChange={(e) => updateTopic(e.target.value as TopicsKeys)}>
             {(Object.keys(TOPICS_KEYS) as TopicsKeysKey[]).map((t) => {
               const disabled = TOPICS_KEYS[t] === TOPICS_KEYS.IMPORTANT_SCHEDULE
@@ -33,16 +35,16 @@ export const Add = () => {
               )
             })}
           </select>
-          <div>
+          <div className="checkbox">
             <input
               type="checkbox"
               checked={state.notification.immediately}
               onChange={(e) => updateCheckbox(e.target.checked)}
               id="immediately"
             />
-            <label htmlFor="immediately">すぐに送る</label>
+            <label htmlFor="immediately">リアルタイムで送る</label>
           </div>
-          <button onClick={() => requestSend()}>送信</button>
+          <button onClick={() => requestSend()}>{buttonLabel}</button>
         </div>
       </details>
     </div>
